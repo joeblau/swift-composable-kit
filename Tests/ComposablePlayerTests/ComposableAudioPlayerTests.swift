87b2f38ff9286cbb6fa771cfaf6eec482694d54f
Joe Blau <im@joeblau.com>
2020-10-07 15:55:36 -0700

    Fix tests

---
+++ Tests/ComposablePlayerTests/ComposableAudioPlayerTests.swift
@@ -0,0 +1,15 @@
+// ComposableAudioPlayerTests.swift
+// Copyright (c) 2020 Joe Blau
+
+import XCTest
+@testable import ComposablePlayer
+
+final class ComposablePlayerTests: XCTestCase {
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
