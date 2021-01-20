d3e850dc6bc02af4181becf6289f34f17cff34f2
Joe Blau <im@joeblau.com>
2021-01-20 10:40:04 -0800

    Update health store

--- Sources/ComposableHealthStore/Interface.swift
+++ Sources/ComposableHealthStore/Interface.swift
@@ -1,54 +1,60 @@
-// ComposableHealthStoreInterface.swift
-// Copyright (c) 2020 Copilot
+// Interface.swift
+// Copyright (c) 2021 Joe Blau
 
 #if canImport(HealthKit)
-import ComposableArchitecture
-import Foundation
-import HealthKit
+    import ComposableArchitecture
+    import Foundation
+    import HealthKit
 
-public enum HealthStoreError: Error {
-    case healthDataNotAvailable
-    case authorizationError
-    case startWatchAppFailure
-}
+    public enum HealthStoreError: Error {
+        case healthDataNotAvailable
+        case authorizationError
+        case startWatchAppFailure
+    }
 
-public struct HealthStoreManager {
-    public enum Action: Equatable {}
+    public struct HealthStoreManager {
+        public enum Action: Equatable {}
 
-    public struct Error: Swift.Error, Equatable {
-        public let error: NSError?
+        public struct Error: Swift.Error, Equatable {
+            public let error: NSError?
 
-        public init(_ error: Swift.Error?) {
-            self.error = error as NSError?
+            public init(_ error: Swift.Error?) {
+                self.error = error as NSError?
+            }
+        }
+
+        // MARK: - Variables
+
+        var create: (AnyHashable) -> Effect<Action, Never> = { _ in _unimplemented("create") }
+
+        var destroy: (AnyHashable) -> Effect<Never, Never> = { _ in _unimplemented("destroy") }
+
+        var requestAuthorization: (AnyHashable, Set<HKSampleType>?, Set<HKObjectType>?) -> Effect<Never, Never> = { _, _, _ in _unimplemented("requestAuthorization") }
+
+        var isHealthAuthorizedFor: (AnyHashable, Set<HKSampleType>, Set<HKObjectType>) -> Effect<Bool, Never> = { _, _, _ in _unimplemented("isHealthAuthorizedFor") }
+
+        var startWatchApp: (AnyHashable, HKWorkoutConfiguration) -> Effect<Bool, Never> = { _, _ in _unimplemented("startWatchApp") }
+
+        // MARK: - Functions
+
+        public func create(id: AnyHashable) -> Effect<Action, Never> {
+            create(id)
+        }
+
+        public func destroy(id: AnyHashable) -> Effect<Never, Never> {
+            destroy(id)
+        }
+
+        public func requestAuthorization(id: AnyHashable, typesToShare: Set<HKSampleType>?, typesToRead: Set<HKObjectType>?) -> Effect<Never, Never> {
+            requestAuthorization(id, typesToShare, typesToRead)
+        }
+
+        public func isHealthAuthorizedFor(id: AnyHashable, typesToShare: Set<HKSampleType>, typesToRead: Set<HKObjectType>) -> Effect<Bool, Never> {
+            isHealthAuthorizedFor(id, typesToShare, typesToRead)
+        }
+
+        public func startWatchApp(id: AnyHashable, configuration: HKWorkoutConfiguration) -> Effect<Bool, Never> {
+            startWatchApp(id, configuration)
         }
     }
-
-    // MARK: - Variables
-
-    var create: (AnyHashable) -> Effect<Action, Never> = { _ in _unimplemented("create") }
-
-    var destroy: (AnyHashable) -> Effect<Never, Never> = { _ in _unimplemented("destroy") }
-
-    var requestAuthorization: (AnyHashable, Set<HKSampleType>?, Set<HKObjectType>?) -> Effect<Never, Never> = { _, _, _ in _unimplemented("requestAuthorization") }
-
-    var startWatchApp: (AnyHashable, HKWorkoutConfiguration) -> Effect<Bool, Never> = { _, _ in _unimplemented("startWatchApp") }
-
-    // MARK: - Functions
-
-    public func create(id: AnyHashable) -> Effect<Action, Never> {
-        create(id)
-    }
-
-    public func destroy(id: AnyHashable) -> Effect<Never, Never> {
-        destroy(id)
-    }
-
-    public func requestAuthorization(id: AnyHashable, typesToShare: Set<HKSampleType>?, typesToRead: Set<HKObjectType>?) -> Effect<Never, Never> {
-        requestAuthorization(id, typesToShare, typesToRead)
-    }
-
-    public func startWatchApp(id: AnyHashable, configuration: HKWorkoutConfiguration) -> Effect<Bool, Never> {
-        startWatchApp(id, configuration)
-    }
-}
 #endif
--- Sources/ComposableHealthStore/Live.swift
+++ Sources/ComposableHealthStore/Live.swift
@@ -1,66 +1,82 @@
-// ComposableHealthStoreLive.swift
-// Copyright (c) 2020 Copilot
+// Live.swift
+// Copyright (c) 2021 Joe Blau
 
 #if canImport(HealthKit)
-import Combine
-import ComposableArchitecture
-import Foundation
-import HealthKit
+    import Combine
+    import ComposableArchitecture
+    import Foundation
+    import HealthKit
 
-public extension HealthStoreManager {
-    static let live: HealthStoreManager = { () -> HealthStoreManager in
+    public extension HealthStoreManager {
+        static let live: HealthStoreManager = { () -> HealthStoreManager in
 
-        var manager = HealthStoreManager()
+            var manager = HealthStoreManager()
 
-        manager.create = { id in
-            Effect.run { subscriber in
-                dependencies[id] = Dependencies(
-                    healthStore: HKHealthStore(),
-                    subscriber: subscriber
-                )
+            manager.create = { id in
+                Effect.run { subscriber in
+                    dependencies[id] = Dependencies(
+                        healthStore: HKHealthStore(),
+                        subscriber: subscriber
+                    )
 
-                return AnyCancellable {
+                    return AnyCancellable {
+                        dependencies[id] = nil
+                    }
+                }
+            }
+
+            manager.destroy = { id in
+                .fireAndForget {
+                    dependencies[id]?.subscriber.send(completion: .finished)
                     dependencies[id] = nil
                 }
             }
-        }
 
-        manager.destroy = { id in
-            .fireAndForget {
-                dependencies[id]?.subscriber.send(completion: .finished)
-                dependencies[id] = nil
-            }
-        }
-
-        manager.requestAuthorization = { id, typesToShare, typeToRead in
-            .fireAndForget {
-                guard HKHealthStore.isHealthDataAvailable() else { return }
-                dependencies[id]?.healthStore.requestAuthorization(toShare: typesToShare, read: typeToRead) { _, _ in
+            manager.requestAuthorization = { id, typesToShare, typeToRead in
+                .fireAndForget {
+                    guard HKHealthStore.isHealthDataAvailable() else { return }
+                    dependencies[id]?.healthStore.requestAuthorization(toShare: typesToShare, read: typeToRead) { _, _ in
+                    }
                 }
             }
-        }
 
-        manager.startWatchApp = { id, configuration in
-            .future { futureCompletion in
-                dependencies[id]?.healthStore.startWatchApp(with: configuration, completion: { success, error in
-                    switch error {
-                    case .some:
-                        return futureCompletion(.success(false))
-                    case .none:
-                        return futureCompletion(.success(true))
-                    }
-                })
+            manager.isHealthAuthorizedFor = { id, typesToShare, typesToRead in
+                .future { futureCompletion in
+                    dependencies[id]?.healthStore.getRequestStatusForAuthorization(toShare: typesToShare, read: typesToRead, completion: { status, error in
+                        switch error {
+                        case .some:
+                            break
+                        case .none:
+                            switch status {
+                            case .unnecessary: return futureCompletion(.success(true))
+                            default: return futureCompletion(.success(false))
+                            }
+                        }
+                    })
+                }
             }
-        }
-        
-        return manager
-    }()
-}
 
-private struct Dependencies {
-    var healthStore: HKHealthStore
-    let subscriber: Effect<HealthStoreManager.Action, Never>.Subscriber
-}
+            manager.startWatchApp = { id, configuration in
+                .future { futureCompletion in
+                    dependencies[id]?.healthStore.startWatchApp(with: configuration, completion: { _, error in
+                        switch error {
+                        case .some:
+                            return futureCompletion(.success(false))
+                        case .none:
+                            return futureCompletion(.success(true))
+                        }
+                    })
+                }
+            }
 
-private var dependencies: [AnyHashable: Dependencies] = [:]
+            return manager
+        }()
+    }
+
+    private struct Dependencies {
+        var healthStore: HKHealthStore
+        let subscriber: Effect<HealthStoreManager.Action, Never>.Subscriber
+    }
+
+    private var dependencies: [AnyHashable: Dependencies] = [:]
 #endif
